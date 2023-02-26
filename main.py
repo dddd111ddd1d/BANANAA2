94fcebe2e8bc3400352b23ea69b03e5a09903e41
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-02-26 07:35:17 +0000

    fixed the mistakes

--- main.py
+++ main.py
@@ -12,18 +12,17 @@ class Cat:
   height = 65
   name = "name"
 
-  def __init__(self, name):
+  def __init__(self, name, height, age):
     self.name = name
     self.height = height
     self.age = age
-    self.color = color
 
   def play_w_homan(self, human):
     human.is_sad = False
     
 
 me = Person("Banana", 5006)
-my_pet = Cat("Vova")
+my_pet = Cat("Vova", 60, 4)
 
 print(me.is_sad)
 
